55c72e8fa3115ba2dc595a0fd5cadaebf6f9767d
hzpeng <huang.zhangpeng@renderg.com>
2017-08-15 22:40:52 +0800

    add cagetory page

--- MagicPress/blog/views.py
+++ MagicPress/blog/views.py
@@ -7,7 +7,7 @@ from flask import render_template, request
 from config import bpdir
 
 from MagicPress.blog import blog
-from .models import Article
+from .models import Article, Category
 from MagicPress import db
 
 
@@ -29,7 +29,7 @@ def index():
 
     page = request.args.get('page', 1, type=int)
     pagination = Article.query.order_by(Article.create_time.desc()).filter_by(state=True).paginate(
-        page, per_page=2, error_out=False
+        page, per_page=3, error_out=False
     )
     articles = pagination.items
     return render_template('blog/index.html', articles=articles, pagination=pagination)
@@ -43,3 +43,13 @@ def article(article_id):
     pre_article = db.session.query(Article).filter(Article.id > article_id).order_by(Article.id.asc()).first()
     return render_template('blog/article.html', article=article, next_article=next_article, pre_article=pre_article)
 
+
+@blog.route('/category', defaults={'id': None})
+@blog.route('/category/<id>', methods=["GET", "POST"])
+def category(id):
+    if not id:
+        categories = Category.query.all()
+        return render_template('blog/categories.html', categories=categories)
+    else:
+        articles = Category.query.filter_by(id=id).first().articles
+        return render_template('blog/category.html', articles=articles)
